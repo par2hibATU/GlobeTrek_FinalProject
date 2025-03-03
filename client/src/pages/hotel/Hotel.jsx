b6ddb04a8acaa14f3a1837381501f4979ab0876c
par2hibATU <parthibbdcox@gmail.com>
2025-03-03 01:10:13 +0000

    Individual hotels primary details are getting fetched from the database

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -6,31 +6,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleArrowLeft, faCircleArrowRight, faCircleXmark, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import Subscribe from '../mailList/Subscribe';
 import { text } from '@fortawesome/fontawesome-svg-core';
+import useFetch from '../../hooks/useFetch';
+import { useLocation } from 'react-router-dom';
 
 export const Hotel = () => {
+  const location = useLocation()
+  const id = location.pathname.split("/")[2];  //it [2] cause remember the URL (/hotels/id), so hotelId is in pos 2
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
-  const photos = [
-    {
-      src: "https://www.theghotel.ie/wp-content/uploads/2019/11/exterior-13-1024x576.jpg"
-    },
-    {
-      src: "https://www.theghotel.ie/wp-content/uploads/2020/09/Luxury-Room-1920x1080-fp_mm-fpoff_0_0.jpg"
-    },
-    {
-      src: "https://www.theghotel.ie/wp-content/uploads/2021/09/The-Atrium-Suite-1440x720-fpoff__.jpg"
-      
-    },
-    {
-      src: "https://www.theghotel.ie/wp-content/uploads/2019/11/linda-evabgelista-bathroom-gallery-01-959x1024.jpg"
-    },
-    {
-      src: "https://www.theghotel.ie/wp-content/uploads/2023/04/gRoom12-1024x683.jpg"
-    },
-    {
-      src: "https://www.theghotel.ie/wp-content/uploads/2023/04/ghotelinteriorSession8756_HDR3-1024x683.jpg"
-    }
-  ]
+
+  const { data, loading, error } = useFetch('/hotels/find/${id}')
+
 const handleOpen = (i)=>{
   setSlideNumber(i);
   setOpen(true);
@@ -48,44 +34,45 @@ const handleMove = (direction) =>{
   return (
     <div>
       <Navbar/>
+      { loading ? ( 
+        "loading"
+      ) : (
       <div className="hotelContainer">
         {open && <div className="slider">
           <FontAwesomeIcon icon={faCircleXmark} className='close' onClick={()=>setOpen(false)}/>
           <FontAwesomeIcon icon={faCircleArrowLeft} className='arrow' onClick={()=>handleMove("l")}/>
           <div className="sliderWrapper">
-            <img src={photos[slideNumber].src} alt="" className='sliderImg'/>
+            <img 
+            src={data.photos[slideNumber]} 
+            alt="" className='sliderImg'/>
           </div>
           <FontAwesomeIcon icon={faCircleArrowRight} className='arrow' onClick={()=>handleMove("r")}/>
         </div>}
         <div className="hotelWrapper">
           <button className='bookNow'>Reserve or Book Now!</button>
-          <h1 className='hotelTitle'>Grand Hotel</h1>
+          <h1 className='hotelTitle'>{data.name}</h1>
           <div className="hotelAddress">
             <FontAwesomeIcon icon={faLocationDot}/>
-            <span>8 Quay Street, H91-EY78, Eyre Square, Galway</span>
+            <span>{data.address}</span>
           </div>
           <span className='hotelDistance'>
-            Excellent Location - 500m from the center
+            Excellent Location -  {data.distance}m from the center
           </span>
           <span className='hotelPriceHighlight'>
-            Book your spot over £75 and Experience the absolute comfort. Get a free coach station Taxi pickup
+            Book your spot over £{data.cheapestPrice} and Experience the absolute comfort. Get a free coach station Taxi pickup
           </span>
           <div className="hotelImages">
-            {photos.map((photo, i) =>(
+            {data.photos?.map((photo, i) =>(
               <div className="hotelImgWrapper">
-                <img onClick={()=>handleOpen(i)} src={photo.src} alt="" className='hotelImg'/>
+                <img onClick={()=>handleOpen(i)} src={photo} alt="" className='hotelImg'/>
               </div>
             ))}
           </div>
           <div className="hotelDetails">
             <div className="hotelDetailsTexts">
-              <h1 className='hotelTitle'>Stay in heart of Galway</h1>
+              <h1 className='hotelTitle'>{data.title}</h1>
               <p className='hotelDesc'>
-              Fabulous is your natural habitat. Swaddled in five-star luxury. 
-            Surrounded by exhilarating design. Steps away from a spirited city, where something wonderful waits around every corner. 
-            Everything you see has been chosen to make you smile. Everyone you meet is waiting to make your day. 
-            Whatever you were doing yesterday, today you’re a VIP. Because today, you’re at The g. For us, five stars has never been about formality. 
-            We know the greatest luxury is getting to be yourself. In all your glory. We could go on and on (and on) about what a special hotel this is. 
+                {data.desc}
               </p>
             </div>
             <div className="hotelDetailsPrice">
@@ -103,6 +90,7 @@ const handleMove = (direction) =>{
           </div>
         </div>
       </div>
+    )}
       <Subscribe/>
       <Footer/>
     </div>
