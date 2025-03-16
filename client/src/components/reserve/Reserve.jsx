657938f09f36b17682cc49ae0a2124406186d622
par2hibATU <parthibbdcox@gmail.com>
2025-03-16 12:48:54 +0000

    Room details are not fetched in the frontend

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,8 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./reserve.css"
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
+import useFetch from "../../hooks/useFetch"
 
 const Reserve = ({ setOpen, hotelId}) => {
+    const {data, loading, error} = useFetch(`hotels/room/${hotelId}`)
     return (
         <div className="reserve">
             <div className="rContainer">
@@ -11,6 +13,15 @@ const Reserve = ({ setOpen, hotelId}) => {
                 onClick={() => setOpen(false)}
                 />
                 <span>Select your Rooms:</span>
+                {data.map(item=>(
+                    <div className="rItem">
+                        <div className="rItemInfo">
+                            <div className="rTitle">{item.title}</div>
+                            <div className="rDesc">{item.desc}</div>
+                            <div className="rMax">Max People: <b>{item.maxPeople}</b></div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
