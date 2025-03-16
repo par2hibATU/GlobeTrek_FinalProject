23feffc737c2e7af62d1c4fc5afdb297dc03e4c9
par2hibATU <parthibbdcox@gmail.com>
2025-03-16 13:19:00 +0000

    room checks are added, and checkbox are available with everyroom with respective hotelid, not properly finished yet thou!

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -2,9 +2,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./reserve.css"
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
 import useFetch from "../../hooks/useFetch"
+import { useState } from "react"
 
 const Reserve = ({ setOpen, hotelId}) => {
-    const {data, loading, error} = useFetch(`hotels/room/${hotelId}`)
+    const [selectedRooms, setSelectedRooms] = useState([])
+    const {data, loading, error} = useFetch(`/hotels/room/${hotelId}`)
+
+
+    const handleSelect = (e) =>{
+        const selected = e.target.checked
+    }
     return (
         <div className="reserve">
             <div className="rContainer">
@@ -18,8 +25,16 @@ const Reserve = ({ setOpen, hotelId}) => {
                         <div className="rItemInfo">
                             <div className="rTitle">{item.title}</div>
                             <div className="rDesc">{item.desc}</div>
-                            <div className="rMax">Max People: <b>{item.maxPeople}</b></div>
+                            <div className="rMax">Max People: <b>{item.maxPeople}</b>
+                            </div>
+                            <div className="rPrice">{item.price}</div>
                         </div>
+                        {item.roomNumbers.map((roomNumber) => (
+                            <div className="room">
+                                <label>{roomNumber.number}</label>
+                                <input type="checkbox" value={roomNumber._id} onChange={handleSelect}/>
+                            </div>
+                        ))}
                     </div>
                 ))}
             </div>
