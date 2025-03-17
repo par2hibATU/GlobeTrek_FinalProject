ad675d35eabd986dadeaa28826f5e3ef323b27f1
par2hibATU <parthibbdcox@gmail.com>
2025-03-17 00:56:00 +0000

    nearly working but not fully set

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,16 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./reserve.css"
-import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
+import { faChessKing, faCircleXmark } from "@fortawesome/free-solid-svg-icons"
 import useFetch from "../../hooks/useFetch"
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { SearchContext } from "../../context/SearchContext.js"
 
 const Reserve = ({ setOpen, hotelId}) => {
     const [selectedRooms, setSelectedRooms] = useState([])
     const {data, loading, error} = useFetch(`/hotels/room/${hotelId}`)
+    const {dates} = useContext(SearchContext)
 
+    const getDatesInRange = (startDate, endDate) => {
+        const start = new Date(startDate)
+        const end = new Date(endDate)
+        const date = new Date(start.getTime());
+        let list = []
 
+        while(data<= end){
+            list.push(new Date(date))
+            date.setDate(date.getDate() + 1)
+        }
+        return list
+    }
     const handleSelect = (e) =>{
-        const selected = e.target.checked
+        const checked = e.target.checked
+        const value = e.target.value
+        setSelectedRooms(checked ? [...selectedRooms, value] : selectedRooms.filter((item) => item !== value))
+    }
+
+    const handleClick = ()  => {
+
     }
     return (
         <div className="reserve">
@@ -37,6 +56,7 @@ const Reserve = ({ setOpen, hotelId}) => {
                         ))}
                     </div>
                 ))}
+                <button onClick={handleClick} className="rButton">Reserve Now!!</button>
             </div>
         </div>
     )
