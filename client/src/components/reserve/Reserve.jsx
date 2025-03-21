b3578f3b00e64259373f90fcc2716671532d7b3b
par2hibATU <parthibbdcox@gmail.com>
2025-03-21 00:26:09 +0000

    just added the room availability

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -1,65 +1,110 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import "./reserve.css"
-import { faChessKing, faCircleXmark } from "@fortawesome/free-solid-svg-icons"
-import useFetch from "../../hooks/useFetch"
-import { useContext, useState } from "react"
-import { SearchContext } from "../../context/SearchContext.js"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
-const Reserve = ({ setOpen, hotelId}) => {
-    const [selectedRooms, setSelectedRooms] = useState([])
-    const {data, loading, error} = useFetch(`/hotels/room/${hotelId}`)
-    const {dates} = useContext(SearchContext)
+import "./reserve.css";
+import useFetch from "../../hooks/useFetch";
+import { useContext, useState } from "react";
+import { SearchContext } from "../../context/SearchContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-    const getDatesInRange = (startDate, endDate) => {
-        const start = new Date(startDate)
-        const end = new Date(endDate)
-        const date = new Date(start.getTime());
-        let list = []
+const Reserve = ({ setOpen, hotelId }) => {
+  const [selectedRooms, setSelectedRooms] = useState([]);
+  const { data, loading, error } = useFetch(`/hotels/room/${hotelId}`);
+  const { dates } = useContext(SearchContext);
 
-        while(data<= end){
-            list.push(new Date(date))
-            date.setDate(date.getDate() + 1)
-        }
-        return list
-    }
-    const handleSelect = (e) =>{
-        const checked = e.target.checked
-        const value = e.target.value
-        setSelectedRooms(checked ? [...selectedRooms, value] : selectedRooms.filter((item) => item !== value))
+  const getDatesInRange = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    const date = new Date(start.getTime());
+
+    const dates = [];
+
+    while (date <= end) {
+      dates.push(new Date(date).getTime());
+      date.setDate(date.getDate() + 1);
     }
 
-    const handleClick = ()  => {
+    return dates;
+  };
 
-    }
-    return (
-        <div className="reserve">
-            <div className="rContainer">
-                <FontAwesomeIcon icon={faCircleXmark}
-                className="rClose"
-                onClick={() => setOpen(false)}
-                />
-                <span>Select your Rooms:</span>
-                {data.map(item=>(
-                    <div className="rItem">
-                        <div className="rItemInfo">
-                            <div className="rTitle">{item.title}</div>
-                            <div className="rDesc">{item.desc}</div>
-                            <div className="rMax">Max People: <b>{item.maxPeople}</b>
-                            </div>
-                            <div className="rPrice">{item.price}</div>
-                        </div>
-                        {item.roomNumbers.map((roomNumber) => (
-                            <div className="room">
-                                <label>{roomNumber.number}</label>
-                                <input type="checkbox" value={roomNumber._id} onChange={handleSelect}/>
-                            </div>
-                        ))}
-                    </div>
-                ))}
-                <button onClick={handleClick} className="rButton">Reserve Now!!</button>
+  const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+
+  const isAvailable = (roomNumber) => {
+    const isFound = roomNumber.unavailableDates.some((date) =>
+      alldates.includes(new Date(date).getTime())
+    );
+
+    return !isFound;
+  };
+
+  const handleSelect = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setSelectedRooms(
+      checked
+        ? [...selectedRooms, value]
+        : selectedRooms.filter((item) => item !== value)
+    );
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+      await Promise.all(
+        selectedRooms.map((roomId) => {
+          const res = axios.put(`/rooms/availability/${roomId}`, {
+            dates: alldates,
+          });
+          return res.data;
+        })
+      );
+      setOpen(false);
+      navigate("/");
+    } catch (err) {}
+  };
+  return (
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="rClose"
+          onClick={() => setOpen(false)}
+        />
+        <span>Select your rooms:</span>
+        {data.map((item) => (
+          <div className="rItem" key={item._id}>
+            <div className="rItemInfo">
+              <div className="rTitle">{item.title}</div>
+              <div className="rDesc">{item.desc}</div>
+              <div className="rMax">
+                Max people: <b>{item.maxPeople}</b>
+              </div>
+              <div className="rPrice">{item.price}</div>
             </div>
-        </div>
-    )
-}
+            <div className="rSelectRooms">
+              {item.roomNumbers.map((roomNumber) => (
+                <div className="room">
+                  <label>{roomNumber.number}</label>
+                  <input
+                    type="checkbox"
+                    value={roomNumber._id}
+                    onChange={handleSelect}
+                    disabled={!isAvailable(roomNumber)}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <button onClick={handleClick} className="rButton">
+          Reserve Now!
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Reserve
+export default Reserve;
