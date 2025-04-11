c51c631ba3726168e3c2ad781ee805d30e16d975
par2hibATU <parthibbdcox@gmail.com>
2025-04-11 11:42:49 +0100

    importing librabries

---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./register.css";
+import Navbar from "../../components/navbar/Navbar_for_Login.jsx";
+import Subscribe from "../mailList/Subscribe";
+import Footer from "../../components/footer/Footer";
