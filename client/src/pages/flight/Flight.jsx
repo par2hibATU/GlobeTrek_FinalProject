02d4506235a381286b7034ff7f1166ab0f07309b
par2hibATU <parthibbdcox@gmail.com>
2025-04-14 11:49:28 +0100

    states are declared for the Flight management boiler plate code

--- client/src/pages/flight/Flight.jsx
+++ client/src/pages/flight/Flight.jsx
@@ -1,3 +1,35 @@
 import Reac, { useState } from "react";
 import axios from "axios";
 import "./flight.css";
+
+const Flight = () => {
+  const [apiChoice, setApiChoice] = useState("oneway"); // defaullt option
+  const [query, setQuery] = useState({
+    from: "",
+    to: "",
+    date: "",
+    returnDate: "",
+  });
+
+  const [resuts, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const apiKey = "";
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormInputs((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setErrorMsg(null);
+
+    let endpoint = " ";
+
+    try
+  }
+
+};
